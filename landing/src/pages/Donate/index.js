f152e864439c8ada74378f42d58223017684e606
Jono Freeman <jfree64@gmail.com>
2020-07-16 12:10:51 -0400

    add scrollability

--- landing/src/pages/Donate/index.js
+++ landing/src/pages/Donate/index.js
@@ -6,7 +6,7 @@ export const Donate = () => {
   return(
     <div className='donate page' >
         <h1 className=''>help support THE CAUSE </h1>
-        <iframe title='donation form' src="https://givebutter.com/embed/c/ridetodc" width="100%" height="615px" style={{maxWidth: "601px"}} name="givebutter" frameBorder="0" scrolling="no" seamless allowpaymentrequest="true"></iframe><script src="https://givebutter.com/js/widget.js"></script>
+        <iframe title='donation form' src="https://givebutter.com/embed/c/ridetodc" width="100%" height="615px" style={{maxWidth: "601px"}} name="givebutter" frameBorder="0" scrolling="yes" seamless allowpaymentrequest="true"></iframe><script src="https://givebutter.com/js/widget.js"></script>
     </div>
     )
 }
