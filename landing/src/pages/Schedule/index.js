6325c636c8b7e18a4bc67e01938945396d2fbf8f
Jono Freeman <jfree64@gmail.com>
2020-07-16 20:06:44 -0400

    add phily signup

--- landing/src/pages/Schedule/index.js
+++ landing/src/pages/Schedule/index.js
@@ -7,6 +7,7 @@ export const Schedule = () => {
     <div className='schedule page' >
         <h1 className=''>Schedule</h1>
         <h2>PHILLY TRAINING RIDE august 1st-august 2nd</h2>
+        <a href='https://docs.google.com/forms/d/1v1D3syMD2uF2dGxaILV0stFqRVZSO84y73wKvEHbxE4/edit'>Click here to sign up</a>
     </div>
     )
 }
